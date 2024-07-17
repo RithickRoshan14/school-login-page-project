30a75c417ff3afb3a73428154ac6ce096bc91b0b
rithick roshan <rithickroshan2002@gmial.com>
2024-07-17 16:04:13 +0530

    login page

---
+++ student script.js
@@ -0,0 +1,24 @@
+function validated() {
+    var username = document.forms["form"]["username"].value;
+    var password = document.forms["form"]["password"].value;
+
+    var username_error = document.getElementById("username_error");
+    var password_error = document.getElementById("password_error");
+
+    username_error.style.display = "none";
+    password_error.style.display = "none";
+
+    if (username == "") {
+        username_error.style.display = "block";
+        return false;
+    }
+    if (password == "") {
+        password_error.style.display = "block";
+        return false;
+    }
+    sessionStorage.setItem("username",username);
+
+    window.location.href="studentlogin.html"
+    return false;
+}
+document.querySelector("form").onsubmit = validated;
